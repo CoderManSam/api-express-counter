4b2ef03981fa49bcc8339d86da79a3cb6d0186b2
Samuel Thomas <samuel.thomas93@ntlworld.com>
2022-08-04 17:42:52 +0100

    adding standard criteria and extension criteria 1 and part of 2 file

---
+++ index.js
@@ -0,0 +1,170 @@
+const express = require("express")
+const morgan = require("morgan")
+const cors = require("cors")
+
+const app = express()
+
+app.use(morgan("dev"))
+app.use(cors())
+app.use(express.json())
+
+const port = 3040
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}/`)
+})
+
+let counter = 5
+
+const counters = [
+    {
+        "name": "john",
+        "counter": 1
+    },
+    {
+        "name": "tom",
+        "counter": 2
+    },
+    {
+        "name": "sam",
+        "counter": 3
+    },
+]
+
+app.get('/counter', (req, res) => {
+
+    console.log(counter)
+
+    res.json({"counter": counter})
+})
+
+app.delete('/counter', (req, res) => {
+
+    counter = 0
+
+    res.json({"counter": counter})
+})
+
+app.post('/counter/increment', (req, res) => {
+
+    counter += 1
+
+    res.status(201).json({"counter": counter})
+})
+
+app.post('/counter/decrement', (req, res) => {
+
+    counter -= 1
+
+    res.status(201).json({"counter": counter})
+})
+
+app.post('/counter/double', (req, res) => {
+
+    counter *= 2
+
+    res.status(201).json({"counter": counter})
+})
+
+app.put('/counter', (req, res) => {
+
+    counter = req.query.value
+
+    res.status(201).json({"counter": counter})
+})
+
+app.get('/counter/:name', (req, res) => {
+
+    const {name} = req.params
+
+    const counterObject = counters.find(counter => counter.name === name);
+
+    const counter = counterObject.counter
+
+    res.json({"counter": counter})
+})
+
+app.delete('/counter/:name', (req, res) => {
+
+    const {name} = req.params
+
+    // const counterObject = counters.find(counter => counter.name === name);
+
+    // console.log(counterObject)
+
+    // counterObject.counter = 0
+
+    // res.json({"counter": counterObject.counter})
+
+    const counterIndex =  counters.findIndex(function (counter) {
+        return counter.name === name;
+    });
+
+    const oldCounter = counters.find(counter => counter.name === name);
+
+    const updatedCounter = {...oldCounter,  
+        "counter": 0
+    }
+
+    counters.splice(counterIndex, 1, updatedCounter)
+
+    const resetCounter = counters[counterIndex].counter
+
+    res.json({"contact": resetCounter})
+})
+
+app.post('/counter/:name/increment', (req, res) => {
+
+    const {name} = req.params
+
+    // const counterObject = counters.find(counter => counter.name === name);
+
+    // console.log(counterObject)
+
+    // // counterObject.counter += 1
+
+    // // res.json({"counter": counterObject.counter})
+
+    const counterIndex =  counters.findIndex(function (counter) {
+        return counter.name === name;
+    });
+
+    const oldCounter = counters.find(counter => counter.name === name);
+
+    const incrementOldCounter = oldCounter.counter + 1
+
+    const updatedCounter = {...oldCounter,  
+        "counter": incrementOldCounter
+    }
+
+    counters.splice(counterIndex, 1, updatedCounter)
+
+    const resetCounter = counters[counterIndex].counter
+
+    res.json({"contact": resetCounter})
+})
+
+// app.post('/counter/:name/decrement', (req, res) => {
+
+//     const {name} = req.params
+
+//     const counterObject = counters.find(counter => counter.name === name);
+
+//     console.log(counterObject)
+
+//     counterObject.counter -= 1
+
+//     res.json({"counter": counterObject.counter})
+// })
+
+// app.post('/counter/:name/double', (req, res) => {
+
+//     const {name} = req.params
+
+//     const counterObject = counters.find(counter => counter.name === name);
+
+//     console.log(counterObject)
+
+//     counterObject.counter *= 2
+
+//     res.json({"counter": counterObject.counter})
+// })
